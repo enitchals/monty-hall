b0af9509927a5ccc1d9f60bc0b901bbabb7dff61
enitchals <ellen.nitchals@gmail.com>
2024-03-09 13:26:15 -0800

    refactor class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Door from './Components/Door.js';
 import Welcome from './Components/Welcome.js';
@@ -9,160 +9,150 @@ const door3 = require('./img/door3.jpg');
 const GOAT = require('./img/the-goat.png');
 // const prize = require('./img/prize.jpg');
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      prizeDoor: null,
-      firstPick: null,
-      revealDoor: null,
-      secondPick: null,
-      switchWin: 0,
-      switchLose: 0,
-      stayWin: 0,
-      stayLose: 0,
-      intro: true,
-    }
-    this.beginSim = this.beginSim.bind(this);
-    this.assignPrize = this.assignPrize.bind(this);
-    this.firstDoorPick = this.firstDoorPick.bind(this);
-    this.stayDoor = this.stayDoor.bind(this);
-    this.switchDoor = this.switchDoor.bind(this);
-    this.tally = this.tally.bind(this);
+const initScores = {
+  switch: {
+    win: 0,
+    lose: 0
+  },
+  stay: {
+    win: 0,
+    lose: 0
+  }
+}
+
+function App(){
+  const [prizeDoor, setPrizeDoor] = useState(null);
+  const [firstPick, setFirstPick] = useState(null);
+  const [revealDoor, setRevealDoor] = useState(null);
+  const [secondPick, setSecondPick] = useState(null);
+  const [switchWin, setSwitchWin] = useState(0);
+  const [switchLose, setSwitchLose] = useState(0);
+  const [stayWin, setStayWin] = useState(0);
+  const [stayLose, setStayLose] = useState(0);
+  const [intro, setIntro] = useState(true);
+
+  useEffect(() => {
+    assignPrize();
+  }, [])
+
+  const beginSim = () => {
+    setIntro(false)
   }
 
-  componentDidMount() {
-    this.assignPrize();
-  }
-
-
-// BEGIN SIMULATION
-  beginSim = () => {
-    this.setState({intro: false});
-  }
-
-// ASSIGNS A PRIZE DOOR
-  assignPrize = () => {
+  const assignPrize = () => {
     const random = Math.ceil(Math.random() * 3);
-    this.setState({prizeDoor: random});
-  };
+    setPrizeDoor(random)
+  }
 
-// RECORDS THE FIRST DOOR THE PLAYER PICKS
-firstDoorPick = (door) => {
-  this.setState({firstPick: door});
-  const doors = [1,2,3];
-  const options = doors.filter(n => n !== door && n !== this.state.prizeDoor);
-  this.setState({revealDoor: options[Math.floor(Math.random()*options.length)]});
-};
+  const firstDoorPick = (door) => {
+    setFirstPick(door);
+    const doors = [1,2,3]
+    const options = doors.filter(n => n !== door && n !== prizeDoor);
+    const doorToReveal = options[Math.floor(Math.random()*options.length)]
+    setRevealDoor(doorToReveal);
+  }
 
-// CONFIRMS THE USER WANTS TO STICK WITH THEIR ORIGINAL DOOR
-  stayDoor = () => {
-    this.setState({secondPick: this.state.firstPick});
-  };
+  const stayDoor = () => {
+    setSecondPick(firstPick)
+  }
 
-// CONFIRMS THE USER WANTS TO SWITCH TO THE OTHER REMAINING DOOR
-  switchDoor = () => {
-    const pick = this.state.firstPick;
-    const reveal = this.state.revealDoor;
-    if (pick !== 1 && reveal !== 1) {this.setState({secondPick: 1})};
-    if (pick !== 2 && reveal !== 2) {this.setState({secondPick: 2})};
-    if (pick !== 3 && reveal !== 3) {this.setState({secondPick: 3})};
-  };
+  const switchDoor = () => {
+    const doors = [1,2,3]
+    const remainingDoor = doors.filter(n => n !== firstPick && n !== revealDoor)
+    setSecondPick(remainingDoor)
+  }
 
-// TALLY THE RESULTS OF THIS ROUND
-  tally = () => {
-    const first = this.state.firstPick;
-    const second = this.state.secondPick;
-    const prize = this.state.prizeDoor;
-    // IF THEY STAYED:
-    if (first === second) {
-      if (second === prize) {
-        this.setState({stayWin: this.state.stayWin+1});
-        return;
+  const tally = () => {
+    // if they stuck with their original choice
+    if (firstPick === secondPick){
+      if (secondPick === prizeDoor) {
+        setStayWin(stayWin + 1)
+      } else {
+        setStayLose(stayLose + 1)
       }
-      this.setState({stayLose: this.state.stayLose+1})
-      return;
     }
-    // IF THEY SWITCHED:
-    if (second === prize) {
-      this.setState({switchWin: this.state.switchWin+1});
-      return;
+    // if they changed doors
+    if (firstPick !== secondPick){
+      if (secondPick === prizeDoor) {
+        setSwitchWin(switchWin + 1)
+      } else {
+        setSwitchLose(switchLose + 1)
+      }
     }
-    this.setState({switchLose: this.state.switchLose+1});
   }
 
-// RESET THE SIMULATION
-  reset = () => {
-    this.tally();    
-    this.setState({
-      prizeDoor: null,
-      firstPick: null,
-      revealDoor: null,
-      secondPick: null
-    });
-    this.assignPrize();
+  const reset = () => {
+    tally();
+    setPrizeDoor(null)
+    setFirstPick(null)
+    setRevealDoor(null)
+    setSecondPick(null)
+    assignPrize()
   }
 
-  render() {
-    return (
-      <div className="container">
-      <div className="App">
-      <div className="Simulation column">
-        {(this.state.intro)
-          ? null
-          : <div className="door-row box">
-            <Door door="1" doorImg = {door1} firstDoorPick = {() => this.firstDoorPick(1)} revealDoor={this.state.revealDoor} secondPick={this.state.secondPick} prizeDoor={this.state.prizeDoor}/>
-            <Door door="2" doorImg = {door2} firstDoorPick = {() => this.firstDoorPick(2)} revealDoor={this.state.revealDoor} secondPick={this.state.secondPick} prizeDoor={this.state.prizeDoor}/>
-            <Door door="3" doorImg = {door3} firstDoorPick = {() => this.firstDoorPick(3)} revealDoor={this.state.revealDoor} secondPick={this.state.secondPick} prizeDoor={this.state.prizeDoor}/>
-            </div>}
+  return(
+    <div>
+      <section className='info'>
         <div className="Text box">
           <div className="title">Monty Hall Simulator</div>
-          {(this.state.firstPick === null)
-            ? (this.state.intro)
-              ?<Welcome begin={this.beginSim}/>
+          {(firstPick === null)
+            ? (intro)
+              ?<Welcome begin={beginSim}/>
               :<div>Pick a door!<br/><br/></div>
-            : (this.state.secondPick === null)
-          ? (<div>You picked door {this.state.firstPick}. Monty tells me there's a goat behind door {this.state.revealDoor}. <br/>
+            : (secondPick === null)
+          ? (<div>You picked door {firstPick}. Monty tells me there's a goat behind door {revealDoor}. <br/>
             Do you want to STAY or SWAP?</div>)
-          : (this.state.secondPick === this.state.prizeDoor)
+          : (secondPick === prizeDoor)
             ? 'You won!'
             : 'You lost!'}
           <div>
-          {(this.state.firstPick !== null && this.state.secondPick === null)
-            ?<button className="buttons" onClick={this.stayDoor}>STAY</button>
+          {(firstPick !== null && secondPick === null)
+            ?<button className="buttons" onClick={stayDoor}>STAY</button>
             : ''}
-          {(this.state.firstPick !== null && this.state.secondPick === null)
-            ?<button className="buttons" onClick={this.switchDoor}>SWAP</button>
+          {(firstPick !== null && secondPick === null)
+            ?<button className="buttons" onClick={switchDoor}>SWAP</button>
             : ''}
-          {(this.state.firstPick !== null && this.state.secondPick !== null)
-            ?<button className="buttons" onClick={this.reset}>TRY AGAIN</button>
+          {(firstPick !== null && secondPick !== null)
+            ?<button className="buttons" onClick={reset}>TRY AGAIN</button>
             : ''}
           </div>
         </div>
+      </section>
+      <section className='simulation'>
+        {(intro)
+          ? null
+          : <div className="door-row box">
+            <Door door="1" doorImg = {door1} firstDoorPick = {() => firstDoorPick(1)} revealDoor={revealDoor} secondPick={secondPick} prizeDoor={prizeDoor}/>
+            <Door door="2" doorImg = {door2} firstDoorPick = {() => firstDoorPick(2)} revealDoor={revealDoor} secondPick={secondPick} prizeDoor={prizeDoor}/>
+            <Door door="3" doorImg = {door3} firstDoorPick = {() => firstDoorPick(3)} revealDoor={revealDoor} secondPick={secondPick} prizeDoor={prizeDoor}/>
+            </div>}
+      </section>
+    </div>
+  )
+}
+
+function Scores({stayWin, switchWin, stayLose, switchLose}) {
+  return (
+    <div className="results box">
+      <img className="goatimg" src={GOAT}/>
+      <div className="results-table">
+        <h3>RESULTS:</h3>
+        <tr>
+          <th>&nbsp;</th>
+          <th>stay</th>
+          <th>swap</th>
+        </tr>
+          <th>win</th>
+          <td>{stayWin}</td>
+          <td>{switchWin}</td>
+        <tr>
+          <th>lose</th>
+          <td>{stayLose}</td>
+          <td>{switchLose}</td>
+        </tr>
       </div>
-      <div className="results box">
-        <img className="goatimg" src={GOAT}/>
-        <div className="results-table">
-          <h3>RESULTS:</h3>
-          <tr>
-            <th>&nbsp;</th>
-            <th>stay</th>
-            <th>swap</th>
-          </tr>
-            <th>win</th>
-            <td>{this.state.stayWin}</td>
-            <td>{this.state.switchWin}</td>
-          <tr>
-            <th>lose</th>
-            <td>{this.state.stayLose}</td>
-            <td>{this.state.switchLose}</td>
-          </tr>
-        </div>
-      </div>
-      </div>
-      </div>
-    );
-  };
+    </div>
+  );
 };
 
 export default App;
